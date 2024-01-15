54a73909ba410376f087aa177d3ff256db694fba
brian masinde <brianmasinde78@gmail.com>
2024-01-14 23:27:11 -0800

    try

--- 0x00-python-hello_world/8-concat_edges.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-str = "object-oriented programming with Python"
-print(str[str.find("programming"):str.find("programming") + 21])
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,1 +1,1 @@
-hello world
+0x00-python-hello_world
